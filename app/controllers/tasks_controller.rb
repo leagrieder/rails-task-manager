f8e0b6452b9ca2cef27ad56c5f1a0698b821eff5
Lea Grieder <lea@grieder.org>
2020-05-19 15:03:42 +0200

    m

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,25 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path(@tasks)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
   get '/tasks/new', to: 'tasks#new'
-  get '/tasks/:id', to: 'tasks#show', as: 'show'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch '/tasks/:id', to: 'tasks#update'
   post '/tasks', to: 'tasks#create', as: 'create'
+  delete '/tasks/:id', to: 'tasks#destroy', as: 'delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
